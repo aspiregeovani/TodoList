4a075a36bfb7b32684c33e84c2228be3f3a56edc
ASPIREGEO-NT\Geovane Martins <geeow.2013@gmail.com>
2023-08-30 22:30:39 -0300

    Arquitetura de Módulos

---
+++ lib/app/core/modules/todo_list_module.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:todo_list_provider/app/core/modules/todo_list_page.dart';
+
+abstract class TodoListModule {
+  TodoListModule({
+    List<SingleChildWidget>? bindings,
+    required Map<String, WidgetBuilder> routers,
+  })  : _bindings = bindings,
+        _routers = routers;
+
+  final List<SingleChildWidget>? _bindings;
+  final Map<String, WidgetBuilder> _routers;
+
+  Map<String, WidgetBuilder> get routers {
+    return _routers.map(
+      (key, pageBuilder) => MapEntry(
+        key,
+        (_) => TodoListPage(
+          bindings: _bindings,
+          page: pageBuilder,
+        ),
+      ),
+    );
+  }
+}
--- lib/app/services/app_widget.dart
+++ lib/app/services/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
@@ -28,6 +29,7 @@ class _AppWidgetState extends State<AppWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List Provider',
+      routes: {...AuthModule().routers},
       home: SplashPage(),
     );
   }
