7d983dd1ddde91f76d84d9c83d7d3580bce22243
ASPIREGEO-NT\Geovane Martins <geeow.2013@gmail.com>
2023-08-31 21:16:07 -0300

    Layout de Login e configurando tema - parte 2

--- lib/app/core/widget/todo_list_logo.dart
+++ lib/app/core/widget/todo_list_logo.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 
 class TodoListLogo extends StatelessWidget {
   const TodoListLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          'assets/logo.png',
-          height: 200,
-        ),
-        Text(
-          'Todo List',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            height: 200,
+          ),
+          Text(
+            'Todo List',
+            style: context.textTheme.headline6,
+          ),
+        ],
+      ),
     );
   }
 }
