d149f9126f5756f433fa22b275bf5cdc475f5746
ASPIREGEO-NT\Geovane Martins <geeow.2013@gmail.com>
2023-09-06 22:02:32 -0300

    Alterando nome do usuário

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
-import 'package:todo_list_provider/app/exceptions/auth_exception.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,23 +7,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: HomeDrawer(),
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.read<AuthProvider>().logout(),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Logout'),
-          ),
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ),
-      ),
+      body: Container(),
     );
   }
 }
