e817136632d6c41ae8aa657a9408ca83d82f45e2
ASPIREGEO-NT\Geovane Martins <geeow.2013@gmail.com>
2023-09-07 21:02:15 -0300

    Módulo de cadastro de task - pt 2

--- lib/app/modules/tasks/tasks_create_page.dart
+++ lib/app/modules/tasks/tasks_create_page.dart
@@ -7,98 +7,112 @@ import 'package:todo_list_provider/app/modules/tasks/widgets/calendar_button.dar
 import 'package:validatorless/validatorless.dart';
 
 class TasksCreatePage extends StatefulWidget {
-  final TasksCreateController _controller;
-
-  TasksCreatePage({
+  const TasksCreatePage({
     Key? key,
     required TasksCreateController controller,
   })  : _controller = controller,
         super(key: key);
 
+  final TasksCreateController _controller;
+
   @override
-  _TasksCreatePageState createState() => _TasksCreatePageState();
+  State<TasksCreatePage> createState() => _TasksCreatePageState();
 }
 
 class _TasksCreatePageState extends State<TasksCreatePage> {
-  final _descriptionEC = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  final _descriptionEC = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+
     DefaultListenerNotifier(
       changeNotifier: widget._controller,
     ).listener(
       context: context,
       successCallback: (notifier, listenerInstance) {
         listenerInstance.dispose();
+
         Navigator.pop(context);
       },
     );
   }
 
   @override
-  void dispose() {
-    super.dispose();
-    _descriptionEC.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
           IconButton(
-            onPressed: () => Navigator.of(context).pop(),
-            icon: Icon(
-              Icons.close,
-              color: Colors.black,
-            ),
-          ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.close,
+                color: Colors.black,
+              ))
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: context.primaryColor,
-          onPressed: () {
-            final formValid = _formKey.currentState?.validate() ?? false;
+        backgroundColor: context.primaryColor,
+        onPressed: () {
+          final formValid = _formKey.currentState?.validate() ?? false;
 
-            if (formValid) {
-              widget._controller.save(_descriptionEC.text);
-            }
-          },
-          label: Text(
-            'Salvar Task',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          )),
+          if (formValid) {
+            widget._controller.save(_descriptionEC.text);
+          }
+        },
+        label: Text(
+          'Salvar Task',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       body: Form(
-          key: _formKey,
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Criar Atividade',
-                    style: context.titleStyle.copyWith(fontSize: 20),
-                  ),
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Criar Atividade',
+                  style: context.titleStyle.copyWith(fontSize: 20),
                 ),
-                SizedBox(height: 30),
-                TodoListField(
-                  label: '',
-                  controller: _descriptionEC,
-                  validator: Validatorless.required('Descrição é obrigatória'),
-                ),
-                SizedBox(height: 20),
-                CalendarButton(),
-              ],
-            ),
-          )),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TodoListField(
+                label: '',
+                controller: _descriptionEC,
+                validator: Validatorless.required('Descrição é obrigatória'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CalendarButton(),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _descriptionEC.dispose();
+
+    super.dispose();
   }
 }
